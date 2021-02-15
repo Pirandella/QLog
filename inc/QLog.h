6679e5589b825a893da4758d8d38c25a04f54061
Sergej <dexxonone@gmail.com>
2021-02-15 11:48:23 +0200

    Comments added
    Signed-off-by: Sergej <dexxonone@gmail.com>

--- inc/QLog.h
+++ inc/QLog.h
@@ -37,6 +37,17 @@ private:
     static std::map<logType, std::string> m_logTypeCharsMap;
     FileHandler *m_logFile = nullptr;
 
+    /* 
+     * Function lonMsgFormat(logType type, std::string logMsg, va_list var_list)
+     * Input:
+     *      type - enum with the log priorities
+     *      msg - format log string
+     *      var_list - variables
+     * Return:
+     *      Formated string
+     * Description:
+     *      Performs log string fomat and returns it to be printed.
+     */
     std::string logMsgFormat(logType type, std::string logMsg, va_list var_list);
 
 public:
@@ -44,9 +55,38 @@ public:
     QLog(std::string tag, bool color);
     ~QLog();
 
+    /* 
+     * Function setLogLevel(logLevel level)
+     * Input:
+     *      level - loggin level
+     * Return:
+     *      void
+     * Description:
+     *      Sets lowest level of logs that will be printed.
+     */
     void setLogLevel(logLevel level);
-
+    /* 
+     * Function log(logType type, std::string msg, ...)
+     * Input:
+     *      type - log level type
+     *      msg - log format
+     *      ... - variables
+     * Retrun:
+     *      void
+     * Description:
+     *      Performs logging to console. ("[TAG]  INFO: <msg>")
+     */
     void log(logType type, std::string msg, ...);
+    /* 
+     * Function log(const char *msg, ...)
+     * Input:
+     *      msg - log format
+     *      ... - variables
+     * Result:
+     *      void
+     * Description:
+     *      Performs loggin to file.
+     */
     void log(const char *msg, ...);
 };
 
